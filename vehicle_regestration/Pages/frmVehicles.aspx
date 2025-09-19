602373fb01f9fba794fba5a2190fba7553575892
Tertius Denis Liebenberg <tertiusliebenberg7@gmail.com>
2025-09-19 10:26:36 +0200

    Updated Vehicles

--- vehicle_regestration/Pages/frmVehicles.aspx
+++ vehicle_regestration/Pages/frmVehicles.aspx
@@ -1,9 +1,89 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="frmVehicles.aspx.cs" Inherits="vehicle_regestration.Pages.frmVehicles" %>
+﻿<%@ Page Title="Vehicle Registration" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="frmVehicles.aspx.cs" Inherits="vehicle_regestration.Pages.frmVehicles" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
-    Vehicle Management
+    Vehicle Registration
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <h2>Contractor Vehicles</h2>
-    <p>Now for the Vehicle page. I would like to have a table that shows (type, regestration number, model and weight in tons and an extra column for buttons) there should be a button that adds a new vehicle and the popup should ask the user for all the information as well as have a dropdown to select a contractor to link to the vehicle. the last column of the table will have an edit button</p>
+    <div class="container mt-4">
+        <h2 class="mb-4">Vehicle Management</h2>
+
+        <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#vehicleModal" onclick="clearVehicleModal();">
+            Add New Vehicle
+        </button>
+
+
+        <asp:GridView ID="gvVehicles" runat="server" CssClass="table table-striped table-bordered"
+            AutoGenerateColumns="False" OnRowCommand="gvVehicles_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="TYPE" HeaderText="Type" />
+                <asp:BoundField DataField="REGESTRATION_NUMBER" HeaderText="Registration Number" />
+                <asp:BoundField DataField="MODEL" HeaderText="Model" />
+                <asp:BoundField DataField="WEIGHT" HeaderText="Weight (tons)" DataFormatString="{0:N2}" />
+                <asp:TemplateField HeaderText="Actions">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="btnEdit" runat="server"
+                            CommandName="EditVehicle"
+                            CommandArgument='<%# Eval("ENTRY_ID") %>'
+                            CssClass="btn btn-sm btn-warning me-1">Edit</asp:LinkButton>
+
+                        <asp:LinkButton ID="btnDelete" runat="server"
+                            CommandName="DeleteVehicle"
+                            CommandArgument='<%# Eval("ENTRY_ID") %>'
+                            CssClass="btn btn-sm btn-danger">Delete</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+
+    <div class="modal fade" id="vehicleModal" tabindex="-1" aria-labelledby="vehicleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="vehicleModalLabel">Add/Edit Vehicle</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <asp:HiddenField ID="hfVehicleID" runat="server" />
+
+                    <div class="mb-3">
+                        <label for="txtType" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="txtType" runat="server" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="txtRegNo" class="form-label">Registration Number</label>
+                        <input type="text" class="form-control" id="txtRegNo" runat="server" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="txtModel" class="form-label">Model</label>
+                        <input type="text" class="form-control" id="txtModel" runat="server" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="txtWeight" class="form-label">Weight (tons)</label>
+                        <input type="number" step="0.01" class="form-control" id="txtWeight" runat="server" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="ddlContractor" class="form-label">Contractor</label>
+                        <asp:DropDownList ID="ddlContractor" runat="server" CssClass="form-select"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <asp:Button ID="btnSaveVehicle" runat="server" Text="Save Vehicle" CssClass="btn btn-primary" OnClick="btnSaveVehicle_Click" />
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+        function clearVehicleModal() {
+            document.getElementById('<%= hfVehicleID.ClientID %>').value = '';
+        document.getElementById('<%= txtType.ClientID %>').value = '';
+        document.getElementById('<%= txtRegNo.ClientID %>').value = '';
+        document.getElementById('<%= txtModel.ClientID %>').value = '';
+        document.getElementById('<%= txtWeight.ClientID %>').value = '';
+        document.getElementById('<%= ddlContractor.ClientID %>').selectedIndex = 0;
+        }
+    </script>
 </asp:Content>
--- vehicle_regestration/Pages/frmVehicles.aspx.cs
+++ vehicle_regestration/Pages/frmVehicles.aspx.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +9,136 @@ namespace vehicle_regestration.Pages
 {
     public partial class frmVehicles : System.Web.UI.Page
     {
+        private string connStr = ConfigurationManager.ConnectionStrings["SizananiDB"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindContractors();
+                BindVehicles();
+            }
+        }
 
+        private void BindContractors()
+        {
+            ddlContractor.Items.Clear();
+            ddlContractor.Items.Add(new ListItem("-- Select Contractor --", ""));
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand("USP_CONTRACTORS_READ", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ddlContractor.Items.Add(new ListItem(reader["CONTRACTOR_NAME"].ToString(),
+                        reader["ENTRY_ID"].ToString()));
+                }
+            }
+        }
+
+        private void BindVehicles()
+        {
+            ClearPopup();
+
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand("USP_VEHICLES_READ", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            gvVehicles.DataSource = dt;
+            gvVehicles.DataBind();
+        }
+
+        private void ClearPopup()
+        {
+            txtType.Value = string.Empty;
+            txtRegNo.Value = string.Empty;
+            txtModel.Value = string.Empty;
+            txtWeight.Value = string.Empty;
+            ddlContractor.SelectedIndex = 0;
+        }
+
+        protected void btnSaveVehicle_Click(object sender, EventArgs e)
+        {
+            int contractorId = string.IsNullOrEmpty(ddlContractor.SelectedValue) ? 0 : Convert.ToInt32(ddlContractor.SelectedValue);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            using (SqlCommand cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+
+                if (string.IsNullOrEmpty(hfVehicleID.Value))
+                {
+                    cmd.CommandText = "USP_VEHICLE_INSERT";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                }
+                else
+                {
+                    cmd.CommandText = "USP_VEHICLE_UPDATE";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ENTRY_ID", Convert.ToInt32(hfVehicleID.Value));
+                }
+
+                cmd.Parameters.AddWithValue("@TYPE", txtType.Value.Trim());
+                cmd.Parameters.AddWithValue("@REGESTRATION_NUMBER", txtRegNo.Value.Trim());
+                cmd.Parameters.AddWithValue("@MODEL", txtModel.Value.Trim());
+                cmd.Parameters.AddWithValue("@WEIGHT", Convert.ToDecimal(txtWeight.Value));
+                cmd.Parameters.AddWithValue("@CONTRACTOR_ID", contractorId > 0 ? (object)contractorId : DBNull.Value);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            BindVehicles();
+        }
+
+        protected void gvVehicles_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int vehicleID = Convert.ToInt32(e.CommandArgument);
+
+            if (e.CommandName == "DeleteVehicle")
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand("USP_VEHICLE_DELETE", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ENTRY_ID", vehicleID);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                BindVehicles();
+            }
+            else if (e.CommandName == "EditVehicle")
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                using (SqlCommand cmd = new SqlCommand("USP_VEHICLES_READ", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@VEHICLE_ID", vehicleID);
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        hfVehicleID.Value = vehicleID.ToString();
+                        txtType.Value = reader["TYPE"].ToString();
+                        txtRegNo.Value = reader["REGESTRATION_NUMBER"].ToString();
+                        txtModel.Value = reader["MODEL"].ToString();
+                        txtWeight.Value = reader["WEIGHT"].ToString();
+                        ddlContractor.SelectedValue = reader["CONTRACTOR_ID"].ToString();
+                    }
+                }
+
+                // Open modal via ScriptManager
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "OpenModal",
+                    "var myModal = new bootstrap.Modal(document.getElementById('vehicleModal')); myModal.show();", true);
+            }
         }
     }
 }
